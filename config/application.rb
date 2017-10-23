b58ecc224ad03320306aa4dbc45d78cafcfbe592
Lenilson Júnior <me@lenilson.codes>
2017-10-23 16:56:36 -0300

    :wrench: application.rb settings

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,20 @@ module Moneys
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = ActiveSupport::TimeZone.new('America/Recife')
+    config.active_record.time_zone_aware_types = [:datetime]
+    config.active_record.default_timezone = :utc # Or :utc
+
+    #config.exceptions_app = self.routes
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :test_unit, fixture: true
+      g.scaffold_stylesheet false
+      g.stylesheets     false
+      g.javascripts     false
+    end
+
   end
 end
