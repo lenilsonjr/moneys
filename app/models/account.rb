9f9112b9e9173c481bcfc9611e99a75c52b16178
Lenilson Júnior <me@lenilson.codes>
2017-10-26 13:05:02 -0300

    :wrench: Melhorias no formulário de contas

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,11 @@
 class Account < ApplicationRecord
   belongs_to :book
 
+  validates_presence_of :name
+  validates_presence_of :kind
+  validates_presence_of :currency
+  validates_presence_of :logo
+  
   def self.available_kinds
     [
       ["Conta Corrente", :checking],
