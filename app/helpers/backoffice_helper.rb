bbef67f1e9ef165f73baf318c13cd4fdb53717f6
Lenilson Júnior <me@lenilson.codes>
2017-10-26 13:25:12 -0300

    :wrench: Exibição dos icones no card

--- app/helpers/backoffice_helper.rb
+++ app/helpers/backoffice_helper.rb
@@ -1,7 +1,7 @@
 module BackofficeHelper
 
   def bank_icons
-    Dir.entries(Rails.root.join('public', 'assets', 'banks')).select {|f| !File.directory? f}.sort_by!{ |m| m }
+    Dir.entries(Rails.root.join('public', 'assets', 'accounts')).select {|f| !File.directory? f}.sort_by!{ |m| m }
   end
 
 end
